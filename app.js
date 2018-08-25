fde2a5bd4e28440d621092e293846bae5ba7750a
Ric <rcalixtos@gmail.com>
2018-08-25 13:05:03 -0300

    aula 25/08/2018

--- app.js
+++ app.js
@@ -11,7 +11,16 @@ var budgetController = (function () {
         this.id = id;
         this.description = description;
         this.value = value;
-    }
+    };
+
+    var calculateTotal = function(type) {
+        var sum = 0;
+        data.allItems[type].forEach(function(cur){
+            sum += cur.value; //lembrete: é igual a sum = sum + cur.value
+        });
+        data.totals[type] = sum;
+
+    };
     
     var data = {
         allItems: {
@@ -21,7 +30,10 @@ var budgetController = (function () {
         totals: {
             exp: 0,
             inc: 0
-        }
+        },
+        budget: 0,
+        percentage: -1 //doesn't exist for the moment
+
     };
 
     return {
@@ -49,6 +61,28 @@ var budgetController = (function () {
             return newItem;
         },
 
+        calculateBudget: function() {
+
+            // calculate total income and expenses
+            calculateTotal('exp');
+            calculateTotal('inc');
+            
+            // calculate the budget: income - expenses
+            data.budget = data.totals.inc - data.totals.exp;
+
+            // calculate the percentage of income that we spent
+            data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100); 
+        },
+
+        getBudget: function() {
+            return {
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage
+            }
+        },
+
         testing: function() {
             console.log(data);
         }
@@ -73,7 +107,7 @@ var UIController = (function() {
                 return{
                 type: document.querySelector(DOMstrings.inputType).value, //Will be either inc or exp
                 description: document.querySelector(DOMstrings.inputDescription).value,
-                value: document.querySelector(DOMstrings.inputValue).value
+                value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
             };
         },
 
@@ -97,6 +131,20 @@ var UIController = (function() {
 
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
+
+        clearFields: function() {
+            var fields, fieldsArr;
+            
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + 
+            DOMstrings.inputValue);
+
+            fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach(function(current, index, array){
+                current.value = '';
+            });
+            fieldsArr[0].focus();
+        },
         getDOMstrings: function(){
             return DOMstrings;
         }
@@ -119,21 +167,37 @@ var controller = (function(budgetCtrl, UICtrl) {
          });
     };
     
+    var updateBudget = function() {
+        // 1. Calculate the budget
+        budgetCtrl.calculateBudget();
+
+        // 2. Return the budget
+        var budget = budgetCtrl.getBudget();
+
+        // 3. DIsplay the budget on the UI
+        console.log(budget);
+    };
 
     var ctrlAddItem = function() {
         var input, newItem;
     // 1. Get the field input data (pegar os dados usados para preencher)
         input = UICtrl.getInput();
+        if (input.description !== '' && !isNaN(input.value) && input.value > 0){
 
+        
     // 2. Add the item to the budget controller (add o item no controle de despesas)
         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
     // 3. Add the item to the UI 
         UICtrl.addListItem(newItem, input.type);
-    // 4. Calculate the budget
-
-    // 5. Display the budget on the UI
-
+    
+    // 4. Clear the fields
+        UICtrl.clearFields(); 
+    
+    // 5. Calculate and update budget
+        updateBudget();
+    }
+    
     };
 
     return {
